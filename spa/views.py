d2ad5fc3f88574676cb60e4b7365b41ad340f264
aleksandrveselov <sanek_ves@mail.ru>
2023-08-24 06:40:55 +0300

    Описание контроллеров

--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from spa.models import Habit
+from spa.serializers import HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """Класс-представление для отображения списка всех привычек текущего пользователя"""
+
+    serializer_class = HabitSerializer  # Сериализатор
+    queryset = Habit.Objects.filter(user=settings.AUTH_USER_MODEL)  # список всех привычек авторизованного пользователя
