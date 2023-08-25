217a24bdf5d7e95d1c8a29f5c780a90336514485
aleksandrveselov <sanek_ves@mail.ru>
2023-08-25 20:05:15 +0300

    Создана кастомная модель пользователя

--- spa/admin.py
+++ spa/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from spa.models import Habit
+
 # Register your models here.
+
+admin.site.register(Habit)  # регистрация модели "привычка" в админ-панели
--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """Модель пользователя"""
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
