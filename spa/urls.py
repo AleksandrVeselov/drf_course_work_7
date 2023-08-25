fab80f2484e3ff148cf013b3df4ac3db13d2685b
aleksandrveselov <sanek_ves@mail.ru>
2023-08-25 19:02:25 +0300

    Описание контроллеров

--- spa/urls.py
+++ spa/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from spa.apps import SpaConfig
 from spa.views import (HabitListAPIView, PublicHabitListAPIView, HabitCreateAPIView, HabitUpdateAPIView,
-                       HabitDeleteAPIView)
+                       HabitDestroyAPIView)
 
-appname = SpaConfig.name
+app_name = SpaConfig.name
 
 urlpatterns = [
     path('', HabitListAPIView.as_view(), name='habit_list'),
     path('public', PublicHabitListAPIView.as_view(), name='public_habit_list'),
     path('create', HabitCreateAPIView.as_view(), name='create_habit'),
     path('update', HabitUpdateAPIView.as_view(), name='update_habit'),
-    path('delete', HabitDeleteAPIView.as_view(), name='delete'),
+    path('delete', HabitDestroyAPIView.as_view(), name='delete'),
 ]
--- spa/views.py
+++ spa/views.py
@@ -7,7 +7,48 @@ from spa.serializers import HabitSerializer
 
 
 class HabitListAPIView(generics.ListAPIView):
-    """Класс-представление для отображения списка всех привычек текущего пользователя"""
+    """Класс-представление для отображения списка привычек текущего пользователя"""
 
     serializer_class = HabitSerializer  # Сериализатор
-    queryset = Habit.Objects.filter(user=settings.AUTH_USER_MODEL)  # список всех привычек авторизованного пользователя
+
+    def get_queryset(self):
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    """Класс-представление для отображения списка всех публичных привычек на основе generics"""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """Класс-представление для создания привычки на основе generics"""
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        """Добавление пользователя создаваемой привычке"""
+
+        new_habit = serializer.save()  # сохранение привычки
+        new_habit.user = self.request.user  # добавляем пользователя
+        new_habit.save()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Класс-представление для обновления привычки на основе generics"""
+    serializer_class = HabitSerializer
+
+    def get_queryset(self):
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Класс-представление для удаления привычки на основе generics"""
+
+    def get_queryset(self):
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
